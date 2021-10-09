5e2a04dd584887386571e7ac0043ceab7172d9e2
Alan Chuang <cchuang2@mail.sfsu.edu>
2021-10-09 17:29:56 +0800

    added db initiation sql and test data insert sql

---
+++ application/rumi/backend/conf/test_data_locations.sql
@@ -0,0 +1,14 @@
+INSERT INTO `rumi-db2`.`location`
+(`location`)
+VALUES
+('Los Angeles');
+
+INSERT INTO `rumi-db2`.`location`
+(`location`)
+VALUES
+('San Francisco');
+
+INSERT INTO `rumi-db2`.`location`
+(`location`)
+VALUES
+('San Diego');
