27072f491a004b4351f7d7f0fe1fe2870559620a
Alan Chuang <cchuang2@mail.sfsu.edu>
2021-10-09 20:31:05 +0800

    add parameters for search API

--- application/rumi/backend/routes/posts.js
+++ application/rumi/backend/routes/posts.js
@@ -2,16 +2,46 @@ var express = require('express');
 var router = express.Router();
 var db = require("../conf/database");
 
-/* GET home page. */
+/* GET posts. */
 // TODO Converted to an async function.
 router.get('/', function(req, res, next) {
   let searchText = req.query.search;
-  // res.send("Hello posts.");
-  let baseSQL = `SELECT * FROM post WHERE caption like ?`;
+  let location = req.query.location;
+  let major = req.query.major;
+  let pricefrom = req.query.pricefrom;
+  let priceto = req.query.priceto;
 
-  let sqlReadySearchText = "%" + searchText + "%"
+  let sqlReadySearchText = "%" + searchText + "%";
 
-  db.execute(baseSQL, [sqlReadySearchText])
+  parameters = [sqlReadySearchText]
+
+  let baseSQL = `SELECT p.* 
+                FROM post p 
+                JOIN user u
+                ON u.id = p.creator_id
+                WHERE p.caption like ? `
+  if (location) {
+    baseSQL += ` AND p.location = ? `;
+    parameters.push(location);
+  }
+  if (major) {
+    baseSQL += ` AND u.major = ? `;
+    parameters.push(major);
+  }
+  if (pricefrom) {
+    baseSQL += ` AND p.price >= ? `;
+    parameters.push(pricefrom);
+  }
+  if (priceto) {
+    baseSQL += ` AND p.price <= ? `;
+    parameters.push(priceto);
+  }
+
+  console.log(parameters);
+  console.log(baseSQL);
+
+
+  db.execute(baseSQL, parameters)
     .then(([results, fields]) => {
       if (results && results.length) {
         res.send({
