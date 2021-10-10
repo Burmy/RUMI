bf6a94eb0ecefac7ccca5d317d24f30e7e4c3f7b
Anmol Burmy <anmolburmy145@gmail.com>
2021-10-10 01:38:17 -0700

    connected frontend to backend

--- application/rumi/frontend/src/App.js
+++ application/rumi/frontend/src/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Team from "./Components/Team/Team";
 import Home from "./Components/Home/Home";
 import Members from "./Components/Team/Members";
+import Posts from "./Components/Posts/Posts";
 
 function App() {
     return (
         <Router>
-            <div>
+            <div className="app">
                 <Navbar />
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/team" exact component={Team} />
+                    <Route path="/post/:id" exact component={Posts} />
                 </Switch>
                 <Route path="/team/:member" exact component={Members} />
             </div>
--- application/rumi/frontend/src/Components/Home/Home.js
+++ application/rumi/frontend/src/Components/Home/Home.js
@@ -1,7 +1,97 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./Home.css";
 
-const Home = () => {
-    return <div>this is home page</div>;
-};
+function Home() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [listOfPosts, setListOfPosts] = useState([]);
+
+    let history = useHistory();
+    useEffect(() => {
+        axios
+            .get(`http://18.190.48.206:3001/posts?search`)
+            .then((response) => {
+                console.log(response.data.results);
+                setListOfPosts(response.data.results);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div className="home">
+            <div class="search">
+                <input
+                    type="text"
+                    class="search-text"
+                    placeholder="Search a post!"
+                    onChange={(event) => {
+                        setSearchTerm(event.target.value);
+                    }}
+                />
+                <button type="submit" class="search-button">
+                    Search
+                </button>
+            </div>
+
+            {/* Connect it to the backend */}
+
+            <div className="category">
+                <div className="search-category">
+                    <p>Select Location: </p>
+                    <select className="search-category-select">
+                        <option>Los Angeles</option>
+                        <option>San Francisco</option>
+                        <option>San Diego</option>
+                        <option>Finance</option>
+                    </select>
+                </div>
+
+                <div className="search-category">
+                    <p>Select Major: </p>
+                    <select className="search-category-select">
+                        <option>Computer Science</option>
+                        <option>English Literature</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className="post-container">
+                {listOfPosts
+                    .filter((val) => {
+                        if (searchTerm === "") {
+                            return val;
+                        } else if (
+                            val.caption.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                            val.description.toLowerCase().includes(searchTerm.toLowerCase())
+                        ) {
+                            return val;
+                        }
+                        return false;
+                    })
+                    .map((value, key) => {
+                        value.created_date = new Date(value.created_date).toDateString();
+                        return (
+                            <div key={value.id}>
+                                <div
+                                    className="post-card"
+                                    onClick={() => {
+                                        history.push(`/post/${value.id}`);
+                                    }}
+                                >
+                                    <div className="post-caption">{value.caption}</div>
+                                    <div className="post-desc">{value.description}</div>
+                                    <div className="post-date">Posted on: {value.created_date}</div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </div>
+    );
+}
 
 export default Home;
