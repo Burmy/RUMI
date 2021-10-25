1fe82ad67e1e5642cf9c431d45c643f39fc65f28
Anmol Burmy <anmolburmy145@gmail.com>
2021-10-24 18:02:41 -0700

    finished upload Post

--- application/rumi/frontend/src/Components/Posts/CreatePost/CreatePost.js
+++ application/rumi/frontend/src/Components/Posts/CreatePost/CreatePost.js
@@ -1,109 +1,177 @@
-// import React from "react";
-// import Axios from "axios";
-// import { Formik, Form, Field, ErrorMessage } from "formik";
+import React, { Component } from "react";
+import Axios from "axios";
+import "./CreatePost.css";
 
-// const CreatePost = () => {
-//     const [hasPosted, setHasPosted] = React.useState(false);
-//     // const { authTokens } = useAuth();
+class CreatePost extends Component {
+    render() {
+        var caption;
+        var description;
+        var price;
+        var creator_id;
+        return (
+            <div className="form-container">
+                <div className="upload-card">
+                    <form>
+                        <p className="form-heading">Post</p>
 
-//     const postListing = (body, { setSubmitting }) => {
-//         const fd = new FormData();
-//         Object.entries(body).forEach(([key, val]) => {
-//             fd.append(key, val);
-//         });
-//         Axios.post("/api/listing", fd)
-//             .then((res) => {
-//                 setHasPosted(true);
-//             })
-//             .catch((error) => {
-//                 console.log(error);
-//                 setSubmitting(false);
-//             });
-//     };
+                        <div class="upload-container">
+                            <div class="upload-image">
+                                <label for="upload">Upload your Image:</label>
+                                <input type="file" id="photo" accept="image/jpg,image/jpeg,image/png" />
+                            </div>
 
-//     const formData = {
-//         street: "",
-//         city: "",
-//         state: "",
-//         description: "",
-//         unit_type: "",
-//         offer_type: "",
-//         bedrooms: "",
-//         bathrooms: "",
-//         building_num: "",
-//         zip_code: "",
-//         cost: "",
-//         sq_footage: "",
-//         lease_length: "",
-//         img_path: "",
-//         full_address: "",
-//         // user_id: authTokens?.id,
-//         listingImage: undefined,
-//     };
+                            <div class="upload-info">
+                                <input
+                                    className="form-input"
+                                    value={caption}
+                                    name="caption"
+                                    placeholder="Enter Your Caption"
+                                    onChange={(word) => {
+                                        caption = word.target.value;
+                                    }}
+                                />
+                                <input
+                                    className="form-input"
+                                    value={description}
+                                    onChange={(des) => {
+                                        description = des.target.value;
+                                    }}
+                                    name="description"
+                                    placeholder="Enter Your Description"
+                                />
 
-//     // define form validation rules
-//     // const page1Validation = Yup.object({
-//     //     state: Yup.string()
-//     //         .matches(/^[A-z]{2}$/, "Must be two letters")
-//     //         .required("Required"),
-//     //     zip_code: Yup.string()
-//     //         .matches(/^[0-9]{5}$/, "Must be 5 digits")
-//     //         .required("Required"),
-//     //     street: Yup.string().required("Required"),
-//     //     city: Yup.string().required("Required"),
-//     //     building_num: Yup.number().positive().integer().required("Required"),
-//     // });
+                                <div class="upload-info-price">
+                                    <input
+                                        className="form-input"
+                                        type="number"
+                                        value={price}
+                                        onChange={(money) => {
+                                            price = money.target.value;
+                                        }}
+                                        name="price"
+                                        placeholder="Enter Your Price($)"
+                                    />
 
-//     // const page2Validation = Yup.object({
-//     //     bedrooms: Yup.number().positive().integer().required("Required"),
-//     //     bathrooms: Yup.number().positive().required("Required"),
-//     //     unit_type: Yup.string().required("Required"),
-//     //     offer_type: Yup.string().required("Required"),
-//     // });
+                                    <select className="form-input" name="location" id="location">
+                                        <option value="0" selected disabled>
+                                            Select a Location
+                                        </option>
+                                        <option value="1">Daly City</option>
+                                        <option value="2">San Francisco</option>
+                                        <option value="3">South San Francisco</option>
+                                        <option value="4">Berkeley</option>
+                                        <option value="5">Oakland</option>
+                                        <option value="6">Alameda</option>
+                                        <option value="7">San Mateo</option>
+                                        <option value="8">San Leandro</option>
+                                    </select>
+                                </div>
 
-//     // const page3Validation = Yup.object({
-//     //     cost: Yup.number().positive().integer().required("Required"),
-//     //     sq_footage: Yup.number().positive().integer().required("Required"),
-//     //     lease_length: Yup.number().positive().integer().required("Required"),
-//     //     listingImage: Yup.mixed().required("Required"),
-//     // });
+                                <div className="upload-info-pref">
+                                    <div>
+                                        <div className="upload-info-pref-heading">Parking Allowed?</div>
+                                        <div className="upload-info-pref-values">
+                                            <input type="radio" id="p1" name="park" value="1" /> Yes
+                                            <input type="radio" id="p2" name="park" value="0" /> No
+                                        </div>
+                                    </div>
 
-//     // if (hasPosted) {
-//     //     return <Redirect to="/account" />;
-//     // }
+                                    <div>
+                                        <div className="upload-info-pref-heading">Pets Allowed?</div>
+                                        <div className="upload-info-pref-values">
+                                            <input type="radio" id="pe1" name="pet" value="1" /> Yes
+                                            <input type="radio" id="pe2" name="pet" value="0" /> No
+                                        </div>
+                                    </div>
 
-//     return (
-//         <div>
-//             <center>
-//                 <h4 className="mt-3">Post a Listing</h4>
-//             </center>
-//             <div className="row container-fluid align-item-center justify-content-center mt-2">
-//                 <div className="col-md-6">
-//                     <div className="card mb-4">
-//                         <div className="card-body">
-//                             <Formik
-//                                 initialValues={{ ...formData }}
-//                                 onSubmit={(values, { setSubmitting }) => {
-//                                     setSubmitting(true);
-//                                     postListing(values, { setSubmitting });
-//                                 }}
-//                             >
-//                                 <WizardStep validationSchema={page1Validation}>
-//                                     <Page1 />
-//                                 </WizardStep>
-//                                 <WizardStep validationSchema={page2Validation}>
-//                                     <Page2 />
-//                                 </WizardStep>
-//                                 <WizardStep validationSchema={page3Validation}>
-//                                     <Page3 />
-//                                 </WizardStep>
-//                             </Formik>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+                                    <div>
+                                        <div className="upload-info-pref-heading">Smoking Allowed</div>
+                                        <div className="upload-info-pref-values">
+                                            <input type="radio" id="s1" name="smoke" value="1" /> Yes
+                                            <input type="radio" id="s2" name="smoke" value="0" /> No
+                                        </div>
+                                    </div>
 
-// export default CreatePost;
+                                    <div>
+                                        <div className="upload-info-pref-heading">Gender Specific?</div>
+                                        <div className="upload-info-pref-values">
+                                            <input type="radio" id="g1" name="gender" value="M" /> Male
+                                            <input type="radio" id="g2" name="gender" value="F" /> Female
+                                            <input type="radio" id="g2" name="gender" value="N" /> Non-Binary
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {/* <div className="upload-info-pref">
+                                <input
+                                    onChange={(create) => {
+                                        creator_id = create.target.value;
+                                    }}
+                                    value={1}
+                                    type="checkbox"
+                                    name="creator_id"
+                                />
+                                id
+                            </div> */}
+                            </div>
+                        </div>
+                    </form>
+                    <div>
+                        <button
+                            type="submit"
+                            className="form-input-btn"
+                            onClick={() => {
+                                if (!caption || !caption.length || !description || !description.length || !price) {
+                                    return alert("You must fill the form to continue.");
+                                } else {
+                                    var photo = document.getElementById("photo");
+                                    var form = new FormData();
+                                    form.append("caption", caption);
+                                    form.append("description", description);
+                                    form.append("price", price);
+                                    form.append("location", document.getElementById("location").value);
+                                    form.append("parking", document.querySelector('input[name="park"]:checked').value);
+                                    form.append("pet", document.querySelector('input[name="pet"]:checked').value);
+                                    form.append("smoking", document.querySelector('input[name="smoke"]:checked').value);
+                                    form.append("gender", document.querySelector('input[name="gender"]:checked').value);
+                                    form.append("creator_id", creator_id);
+                                    form.append("photo", photo.files[0]);
+
+                                    console.log(
+                                        form.getAll("caption"),
+                                        form.getAll("description"),
+                                        form.getAll("price"),
+                                        form.getAll("location"),
+                                        form.getAll("parking"),
+                                        form.getAll("pet"),
+                                        form.getAll("smoking"),
+                                        form.getAll("gender"),
+                                        form.getAll("creator_id"),
+                                        form.getAll("photo")
+                                    );
+                                    Axios.post("http://18.190.48.206:3001/posts/", form, {
+                                        headers: { "content-type": "multipart/form-data" },
+                                    })
+                                        .then((result) => {
+                                            console.log(result);
+                                            if (result.data.success) {
+                                                alert("Successfully Posted");
+                                            } else {
+                                                alert("Post Failure Occurred");
+                                            }
+                                        })
+                                        .catch((error) => {
+                                            console.log(error.response);
+                                        });
+                                }
+                            }}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default CreatePost;
