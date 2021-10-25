e35493f9fbc689151b1a183dd4664e6d5f559d0a
Alan Chuang <cchuang2@mail.sfsu.edu>
2021-10-25 21:00:23 +0800

    added API: get user by id

--- application/rumi/backend/models/users.js
+++ application/rumi/backend/models/users.js
@@ -1,4 +1,15 @@
 var db = require("../conf/database");
 const UserModel = {};
 
+UserModel.getById = (id) => {
+    let baseSQL = `SELECT * FROM user WHERE id = ?;`;
+    
+    return db
+    .execute(baseSQL, [id])
+    .then(([results, fields]) => {
+      return Promise.resolve(results);
+    })
+    .catch((err) => Promise.reject(err));
+}
+
 module.exports = UserModel;
--- application/rumi/backend/routes/users.js
+++ application/rumi/backend/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var UserModel = require("../models/users");
 
-router.get('/', function(req, res, next) {
-  res.send('users API path');
+router.get("/", function (req, res, next) {
+  let id = req.query.id;
+
+  if (!id) {
+    return res.status(400).send({ message: "id should not be null" });
+  }
+
+  UserModel.getById(id)
+    .then((results) => {
+      if (results && results.length) {
+        res.send({
+          resultsStatus: "info",
+          message: `${results.length} result found`,
+          results: results,
+        });
+      } else {
+        res.send({
+          resultsStatus: "info",
+          message: `Cannot find any user by id ${id}`,
+        });
+      }
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
