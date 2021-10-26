441641e5043dde41eb89a5e0fcd926f18b179837
jmiranda7 <jmiranda7@mail.sfsu.edu>
2021-10-25 18:07:03 -0700

    User functions testing

--- application/rumi/backend/models/users.js
+++ application/rumi/backend/models/users.js
@@ -1,4 +1,59 @@
 var db = require("../conf/database");
 const UserModel = {};
 
+UserModel.search = (
+    school,
+    major,
+    pets,
+    smoker,
+    gender
+) => {
+    return null;
+}
+
+UserModel.create = (
+    id,
+    username,
+    password,
+    last_name,
+    first_name,
+    email,
+    phone,
+    birthday,
+    school,
+    major,
+    smoker,
+    pets,
+    language,
+    interests,
+    hobbies
+) => {
+    let baseSQL = `INSERT INTO post 
+  (caption, description, photo, thumbnail, location, price, parking, pet, smoking, gender, creator_id, deleted) 
+  VALUES 
+  (?,?,?,?,?,?,?,?,?,?,?,0);`;
+    return db
+        .execute(baseSQL, [
+            id,
+            username,
+            password,
+            last_name,
+            first_name,
+            email,
+            phone,
+            birthday,
+            school,
+            major,
+            smoker,
+            pets,
+            language,
+            interests,
+            hobbies
+        ])
+        .then(([results, fields]) => {
+            return Promise.resolve(results);
+        })
+        .catch((err) => Promise.reject(err));
+};
+
 module.exports = UserModel;
