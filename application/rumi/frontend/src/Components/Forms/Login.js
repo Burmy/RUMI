f710fcc386da5e197416f7eb93aeb54cf106ba7c
Anmol Burmy <anmolburmy145@gmail.com>
2021-10-26 00:52:08 -0700

    working on login

--- application/rumi/frontend/src/Components/Forms/Login.js
+++ application/rumi/frontend/src/Components/Forms/Login.js
@@ -6,12 +6,18 @@ import "./Form.css";
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
+    Axios.defaults.withCredentials = true;
     const login = () => {
-        // const data = { username: username, password: password };
-        // Axios.post("").then((response) => {
-        //     console.log(response.data);
-        // });
+        const data = { username: username, password: password };
+        Axios.post("http://18.190.48.206:3001/users/login", data)
+            .then((response) => {
+                console.log(response.data);
+                console.log(response.headers);
+            })
+            .catch((error) => {
+                console.log(data);
+                console.log(error, error.message, error.response);
+            });
     };
     return (
         <div className="form-container">
@@ -30,7 +36,7 @@ const Login = () => {
 
                     <input
                         className="form-input"
-                        type="password"
+                        type="text"
                         name="password"
                         placeholder="Enter your password"
                         onChange={(e) => {
@@ -38,8 +44,8 @@ const Login = () => {
                         }}
                     />
 
-                    <button className="form-input-btn" onClick={login} type="submit">
-                        Sign Up
+                    <button className="form-input-btn" onClick={login} type="button">
+                        Log in
                     </button>
                     <p className="form-input-login">
                         Not a member yet? Sign up{" "}
--- application/rumi/frontend/src/Components/Forms/Registeration.js
+++ application/rumi/frontend/src/Components/Forms/Registeration.js
@@ -4,38 +4,50 @@ import { Link } from "react-router-dom";
 import Axios from "axios";
 import React from "react";
 import "./Form.css";
+import Major from "../Posts/Listings/CategoryLists/Major";
 
 const Registeration = () => {
     const initialValues = {
         username: "",
         email: "",
-        password1: "",
-        password2: "",
+        password: "",
+        description: "",
+        gender: "",
+        school: "",
+        major: "",
+        smoker: "",
+        pets: "",
     };
 
     //Yup npm package used to do form validation
     const validationSchema = Yup.object().shape({
-        username: Yup.string()
-            .min(3)
-            .max(15)
-            .required("✖ You must create a Username")
-            .matches(/^[a-zA-Z]+/g, "✖ Username must start with a character (a-z or A-Z)"),
+        // username: Yup.string()
+        //     .min(3)
+        //     .max(15)
+        //     .required("✖ You must create a Username")
+        //     .matches(/^[a-zA-Z]+/g, "✖ Username must start with a character (a-z or A-Z)"),
         email: Yup.string().required("✖ You must enter an Email").email("Email must be valid"),
-        password1: Yup.string()
-            .required("✖ You must enter a Password")
-            .matches(
-                /^(?=.*[A-Z])(?=.*[0-9])(?=.*[(/*-+!@#$^&*)])/g,
-                "✖ Must contain at least 1 upper case letter and 1 number and 1 of the following special characters ( / * - + ! @ # $ ^ & * )."
-            ),
-        password2: Yup.string()
-            .required("✖ You must enter a Password")
-            .oneOf([Yup.ref("password1"), null], "✖ Passwords must match"),
+        // password: Yup.string()
+        //     .required("✖ You must enter a Password")
+        //     .matches(
+        //         /^(?=.*[A-Z])(?=.*[0-9])(?=.*[(/*-+!@#$^&*)])/g,
+        //         "✖ Must contain at least 1 upper case letter and 1 number and 1 of the following special characters ( / * - + ! @ # $ ^ & * )."
+        //     ),
+        // password2: Yup.string()
+        //     .required("✖ You must enter a Password")
+        //     .oneOf([Yup.ref("password1"), null], "✖ Passwords must match"),
     });
-
+    // Axios.defaults.withCredentials = true;
     const onSubmit = (data) => {
-        // Axios.post("http://localhost:3001/???????", data).then((response) => {
-        //     console.log("IT WORKED");
-        // });
+        Axios.post("http://18.190.48.206:3001/users/registration", data)
+            .then((response) => {
+                console.log("IT WORKED");
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(data);
+                console.log(error, error.message, error.response);
+            });
     };
     return (
         <div className="form-container">
@@ -48,11 +60,58 @@ const Registeration = () => {
                     <Field className="form-input" name="email" placeholder="Enter Your Email" />
                     <ErrorMessage className="form-error" name="email" component="span" />
 
-                    <Field className="form-input" type="password" name="password1" placeholder="Enter Password" />
-                    <ErrorMessage className="form-error" name="password1" component="span" />
+                    <Field className="form-input" type="text" name="password" placeholder="Enter Password" />
+                    <ErrorMessage className="form-error" name="password" component="span" />
 
-                    <Field className="form-input" type="password" name="password2" placeholder="Confirm Password" />
-                    <ErrorMessage className="form-error" name="password2" component="span" />
+                    <Field className="form-input" type="text" name="description" placeholder="Enter Description" />
+
+                    <div role="group" aria-labelledby="my-radio-group">
+                        <label>
+                            <Field type="radio" name="gender" value="M" />
+                            Male
+                        </label>
+                        <label>
+                            <Field type="radio" name="gender" value="F" />
+                            Female
+                        </label>
+                        <label>
+                            <Field type="radio" name="gender" value="N" />
+                            Non-Binary
+                        </label>
+                    </div>
+
+                    <Field className="form-input" name="school" placeholder="Enter School" />
+
+                    <Field className="form-input" name="major" placeholder="Enter Major" />
+
+                    <div role="group" aria-labelledby="my-radio-group">
+                        Do you Smoke?
+                        <label>
+                            <Field type="radio" name="smoker" value="1" />
+                            Yes
+                        </label>
+                        <label>
+                            <Field type="radio" name="smoker" value="0" />
+                            No
+                        </label>
+                    </div>
+
+                    <div role="group" aria-labelledby="my-radio-group">
+                        Have any pets?
+                        <label>
+                            <Field type="radio" name="pets" value="1" />
+                            Yes
+                        </label>
+                        <label>
+                            <Field type="radio" name="pets" value="0" />
+                            No
+                        </label>
+                    </div>
+
+                    {/* <Major /> */}
+
+                    {/* <Field className="form-input" type="password" name="password2" placeholder="Confirm Password" />
+                    <ErrorMessage className="form-error" name="password2" component="span" /> */}
                     <button className="form-input-btn" type="submit">
                         Submit
                     </button>
