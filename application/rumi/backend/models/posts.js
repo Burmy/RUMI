11b8c713aea721a3be32ad949f341562ff497ffd
Alan Chuang <cchuang2@mail.sfsu.edu>
2021-11-01 17:53:25 +0800

    corrected room detail page's username

--- application/rumi/backend/models/posts.js
+++ application/rumi/backend/models/posts.js
@@ -68,7 +68,7 @@ PostModel.search = (
     parameters.push(page);
     parameters.push(size);
   } else {
-    baseSQL += ` LIMIT 20 `;
+    baseSQL += ` LIMIT 100 `;
   }
 
   return db
@@ -80,8 +80,12 @@ PostModel.search = (
 };
 
 PostModel.queryById = (id) => {
-  let baseSQL = `SELECT * FROM post WHERE id = ?;`;
-  
+  let baseSQL = `SELECT p.*, u.username 
+    FROM post p  
+    JOIN user u 
+    ON p.creator_id = u.id 
+    WHERE p.id = ?; `;
+
   return db
     .execute(baseSQL, [id])
     .then(([results, fields]) => {
