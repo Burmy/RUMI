a365e96ff0a4809191df2f887e00a7bf8007854f
hthoch189 <hthoch189@gmail.com>
2021-11-02 20:04:28 -0700

    added google maps api keys

--- application/rumi/frontend/src/Components/Map/Map.js
+++ application/rumi/frontend/src/Components/Map/Map.js
@@ -1,18 +1,49 @@
-import React from "react";
-import { Link, useRouteMatch } from "react-router-dom";
-import "./MapContainer.js";
-import "./mapStyles.js";
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-const Map = () => {
-    const { url } = useRouteMatch();
-
-    return (
-        <div>
-            <div id="heading">
-                <p>Find Roommates on the Map</p>
-            </div> 
-        </div>
-    );
+const containerStyle = {
+  width: '1900px',
+  height: '800px'
 };
 
-export default Map;
+const center = {
+  lat: 37.7226925146223, 
+  lng: -122.4782858436136
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDtRA5WpTazOG4sjv1pv7cRb3WQaJ5JZ8o"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? ( 
+
+
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {  
+  }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default Map
