90b0ba8004853ed071625633f7af7d32394aa369
rimanov <rimanov@mail.sfsu.edu>
2021-11-04 02:11:41 -0700

    GET/comments working

--- application/rumi/backend/routes/comments.js
+++ application/rumi/backend/routes/comments.js
@@ -3,7 +3,31 @@ var router = express.Router();
 var CommentModel = require("../models/comments");
 
 router.get('/', function(req, res, next) {
-  res.send('comments API path');
+  let id = req.query.id;
+  let creator_id = req.query.creator_id;
+  let post_id = req.query.post_id;
+
+  CommentModel.search(
+    id,
+    creator_id,
+    post_id,
+  )
+    .then((results) => {
+      if (results && results.length) {
+        res.send({
+          resultsStatus: "info",
+          message: `${results.length} comment found`,
+          results: results,
+        });
+      } else {
+        res.send({
+          resultsStatus: "info",
+          message: `0 comment found`,
+        });
+      }
+    })
+    .catch((err) => next(err));
+
 });
 
 module.exports = router;
