d63fb181e53ddf97dde970007daf7b5a8f0995bd
rimanov <rimanov@mail.sfsu.edu>
2021-11-04 19:50:24 -0700

    create comment (POST) implemented

--- application/rumi/backend/routes/comments.js
+++ application/rumi/backend/routes/comments.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var CommentModel = require("../models/comments");
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let id = req.query.id;
   let creator_id = req.query.creator_id;
   let post_id = req.query.post_id;
@@ -23,6 +23,42 @@ router.get('/', function(req, res, next) {
         res.send({
           resultsStatus: "info",
           message: `0 comment found`,
+        });
+      }
+    })
+    .catch((err) => next(err));
+
+});
+
+router.post("/", function (req, res, next) {
+  let text = req.body.text;
+  let post_id = req.body.post_id;
+  let creator_id = req.body.creator_id;
+
+  if (!text || !text.length) {
+    return res.status(400).send({ message: "Comment should not be null" });
+  }
+  if (!post_id || !post_id.length) {
+    return res.status(400).send({ message: "post_id should not be null" });
+  }
+  if (!creator_id || !creator_id.length) {
+    return res.status(400).send({ message: "creator_id should not be null" });
+  }
+
+  return CommentModel.create(
+    text,
+    post_id,
+    creator_id
+  )
+    .then((results) => {
+      if (results && results.affectedRows) {
+        res.send({
+          id: results.insertId,
+          message: `Comment is created`,
+        });
+      } else {
+        res.status(400).send({
+          message: `Comment creation failed`,
         });
       }
     })
