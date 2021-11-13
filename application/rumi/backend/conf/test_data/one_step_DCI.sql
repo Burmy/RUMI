6885ce1e1a6ea14b5500d8e9d1f988b2d63f396e
Alan Chuang <cchuang2@mail.sfsu.edu>
2021-11-13 12:22:23 +0800

    modified test data

--- application/rumi/backend/conf/test_data/one_step_DCI.sql
+++ application/rumi/backend/conf/test_data/one_step_DCI.sql
@@ -508,8 +508,9 @@ INSERT INTO `rumi-db2`.`post`
 `deleted`)
 VALUES
 (
-'The Best House in California',
-'Near supermarkets',
+'JTHAVN Joshua Tree Remote Desert Bubble Stargazing',
+'Unique desert bubble is located in a private 30 acres lot! ~13 miles from Joshua Tree National Park in Joshua Tree, California!
+If you love traveling, camping, uniqueness, nature, adventure and experience the magic of the desert, the bubble is a purposefully designed tent with intentional features to help you maximize your travel experiences. Comfortable, luxurious and right in the middle of the desert. Experience the best of both world.',
 1000,
 1,
 'image1.jpeg',
@@ -540,12 +541,14 @@ INSERT INTO `rumi-db2`.`post`
 `deleted`)
 VALUES
 (
-'Great in California',
-'SF',
+'Glass cottage with Hot tub "Blár"',
+'Welcome to Glass cottages Iceland.
+
+We are located on a lava desert in the south of Iceland. 5 minutes from the small town of Hella, close to all the popular attractions that southern Iceland has to offer, but also in a secret and secluded location.',
 800,
 2,
-'image3.jpeg',
-'thumbnail-image3.jpeg',
+'image2.jpeg',
+'thumbnail-image2.jpeg',
 1,
 0,
 1,
@@ -571,12 +574,12 @@ INSERT INTO `rumi-db2`.`post`
 `deleted`)
 VALUES
 (
-'Beach House',
-'Near beach',
+'Invisible House Joshua Tree - Skyscraper with Pool',
+'Quite simply, Invisible House is the most spectacular house in Joshua Tree. As seen in design and lifestyle publications worldwide, this mirrored 22 story horizontal skyscraper virtually disappears into the vast desert landscape. The luxurious 100-foot indoor swimming pool contrasts with the High Desert surroundings. The 90 acre property has its own 4000ft mountain and shares half a mile border with the National Park.',
 900,
 3,
-'image2.jpeg',
-'thumbnail-image2.jpeg',
+'image3.jpeg',
+'thumbnail-image3.jpeg',
 1,
 1,
 0,
